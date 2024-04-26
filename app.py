b633413af881b050ddeca0edf4f7018cefd49c77
Bertil1212 <andreasbertilsson1@gmail.com>
2024-04-26 11:34:15 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import flask
+
+
+
+from flask import Flask, render_template, redirect, url_for, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.215', port=5500, debug=True)
---
+++ static/index/index.js
@@ -0,0 +1,46 @@
+var id = 0;
+function ClickNav(id){
+    /*
+    var lines = document.getElementsByClassName("underscore");
+    for (var i = 0; i < lines.length; i++){
+        lines[i].classList.add("hidden");
+    }
+    lines[id].classList.remove("hidden");
+    */
+
+    //Move the carusell
+    var offset = -100 * id;
+    var movestr = offset + "vw";
+    var carousell = document.getElementById("carousell");
+    var current = carousell.style.left.substring(0, carousell.style.left.length -2)
+    if (current == null){
+        current = 0
+    }
+    current = current /-100
+
+    var diff = Math.abs(id - current)
+    var final = diff + 0.7
+    carousell.style.transition = " left " + 0.3*diff+"s"
+    carousell.style.left = movestr;
+
+    //Move the navbar icon
+    var line = document.getElementById("underscore");
+    var screedwidth = window.innerWidth;
+    var offset = (id+1)*screedwidth/5-25;
+
+    var tovw = (offset/screedwidth)*100;
+   
+    line.style.left = tovw+"vw";
+    
+}
+//The nav icon gets abit out of line when the screen is updated.
+function UpdateScreenWidth(){
+    var line = document.getElementById("underscore");
+    var screedwidth = window.innerWidth;
+    var offset = (id+1)*screedwidth/5-25;
+    var tovw = (offset/screedwidth)*100;
+    line.style.left = tovw+"vw";  
+}
+window.onresize = UpdateScreenWidth;
+
+ClickNav(3);
