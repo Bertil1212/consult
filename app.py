3707c17ed08716491d67fad6d171e0d5440ded22
Bertil1212 <andreasbertilsson1@gmail.com>
2024-04-27 12:18:10 +0200

    Was working with nginx

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-import flask
+from flask import Flask, render_template, redirect, url_for
+from flask import request
 
-
-
-from flask import Flask, render_template, redirect, url_for, request
-import requests
 
 app = Flask(__name__)
 
-
-@app.route("/consult")
+@app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route('/consult')
+def consult():
+    return "Right consult"
 
+
+#-------------------------------Main
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5501, debug=True)
+    
+    app.run(host='0.0.0.0', port=5001, debug=True)
+    
